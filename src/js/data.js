e440fb92ccb89a8b7afae195063aa1fccc3ee041
jfxer <searsiyr@gmail.com>
2017-11-06 08:58:12 +0300

    added main page info and background image for main

---
+++ src/js/data.js
@@ -0,0 +1,12 @@
+var data = new Object();
+data.main = new Object();
+/**
+main page data. 
+we should use 1 title for all pages?
+
+*/
+//data.main.title = "What we do?";
+data.main.h1 = "What we do?";
+data.main.paragraphs = ["Software Development", "Software Maintenance","Internet of things", "handsome solutions"]
+// it is background image.
+data.main.image = "main_page_background.png"
